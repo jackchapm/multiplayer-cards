2deb5ceec7e5ced84e74d74d3e02fee9052805b6
Jack Chapman <chapmanjack0777@gmail.com>
2025-05-08 22:44:45 +0100

    chore: delete korge mobile app

--- mobile/src/commonMain/kotlin/com/jackchap/multiplayercards/ui/Utils.kt
+++
@@ -1,8 +0,0 @@
-package com.jackchap.multiplayercards.ui
-
-import korlibs.korge.view.*
-
-fun Container.linearInterpolate(start: Double, end: Double, ratio: Float): Double {
-    return start + (end - start) * ratio
-}
-
